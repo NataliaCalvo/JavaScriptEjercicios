03279e69506fb6ac8e20ef6f45d37a6c10f4a2d3
Natalia Calvo Torel <116891081+NataliaCalvo@users.noreply.github.com>
2022-11-22 22:38:07 +0100

    Delete ejercicio3.js

--- ejercicio3.js
+++
@@ -1,4 +0,0 @@
-//Escribe un programa que pida el nombre del usuario con un input y escriba un texto que diga “Hola <nombre-de-usuario>”
-var nombre=prompt("Hola");
-document.write("Hola, " + nombre);
-
