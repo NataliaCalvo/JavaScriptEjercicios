93f3bba8830cb20a50677778b4c5dc66d2e38fce
Natalia Calvo Torel <116891081+NataliaCalvo@users.noreply.github.com>
2022-11-22 22:38:15 +0100

    Delete ejercicio2.js

--- ejercicio2.js
+++
@@ -1,9 +0,0 @@
-//Escribe un programa que escriba en la pantalla “La suma de 3 + 5 es <resultado>”.
-function printMessage() {
-    const div = document.getElementById("resultado");
-    console.log(div);
-    resultado= 8;
-    div.innerHTML = `<h2>La suma de 3+5 es ${resultado}.</h2>`;
-}
-
-printMessage ();
