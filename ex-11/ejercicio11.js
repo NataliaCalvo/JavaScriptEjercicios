e85b6a9027b33b83e97e173e8fa0c7aa4cc16aa4
Natalia Calvo Torel <116891081+NataliaCalvo@users.noreply.github.com>
2022-12-13 13:50:17 +0100

    Delete ex-11 directory

--- ex-11/ejercicio11.js
+++
@@ -1,6 +0,0 @@
-var n1 = prompt("Escribe un número");
-if (n1 % 2 === 0 || n1 % 3 === 0 || n1 %5 ===0 || n1 % 7 === 0) {
-    document.write("El número es divisible por 2, 3, 5 o 7");
- } else {
-        document.write("El número no es divisible por 2, 3, 5 o 7");
-    }
