5782c2ee807f1feca8304b850b1f022fa24af111
Natalia Calvo Torel <116891081+NataliaCalvo@users.noreply.github.com>
2022-12-27 14:19:42 +0100

    Add files via upload

---
+++ Ejercicios Java Script/Ex-10/ejercicio10.js
@@ -0,0 +1,8 @@
+var text = prompt("Escribe una frase");
+var nText = text.length;
+var i;
+for (i = 0; i < nText; i++) {
+if (text.substr(i,1) === "a" || text.substr(i,1) === "e" || text.substr(i,1) === "i" || text.substr(i,1) === "o" || text.substr(i,1) === "u"|| text.substr(i,1) === "A" || text.substr(i,1) === "E" || text.substr(i,1) === "I" || text.substr(i,1) === "O" || text.substr(i,1) === "U"|| text.substr(i,1) === "Á" || text.substr(i,1) === "É" || text.substr(i,1) === "Í" || text.substr(i,1) === "Ó" || text.substr(i,1) === "Ú") {
+document.write(text.substr(i,1));
+}
+}
---
+++ Ejercicios Java Script/Ex-9/ejercicio9.js
@@ -0,0 +1,9 @@
+var text = prompt("Escriba una frase:");
+var nText = text.length;
+letra = "aeiouAEIOU"
+contador = 0;
+for (i=0 in frase, i==letra, i++){
+    if (i == letra);
+    contador += 1
+}
+    print(`"Las vocales aparecen" $[contador]  "veces en la frase" $[frase]`)
