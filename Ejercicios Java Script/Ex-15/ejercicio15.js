55354e25e61020e32ae6f7941abf85a0aaa48ad6
Natalia Calvo Torel <116891081+NataliaCalvo@users.noreply.github.com>
2022-12-27 14:27:12 +0100

    Add files via upload

---
+++ Ejercicios Java Script/Ex-15/ejercicio15.js
@@ -0,0 +1,10 @@
+var n1 = prompt("Escribe un número");
+var i = 2;
+while (n1 % i !== 0 && i < n1 / 2 ) {
+i++;
+}
+if (n1 % i !== 0) {
+document.write("El número es primo");
+} else {
+document.write("El número no es primo");
+}
